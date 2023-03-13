32aa57f87c642e921f349e222fd16870ff4a9424
Kosova12345 <127774297+Kosova12345@users.noreply.github.com>
2023-03-13 20:02:48 +0100

    update fb

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     password: 'realestate',
+    port: 3306,
     database: 'BH-ESTATE'
 });
